b73002719b30db41f97fdd4e095365e057c4711f
chagoy <angelochagoy@gmail.com>
2018-05-09 09:07:39 -0700

    delete keys

--- object-drills.js
+++ object-drills.js
@@ -45,3 +45,18 @@ function personMaker() {
   return person.fullName();
 }
 // console.log(personMaker());
+
+function keyDeleter(obj) {
+  delete obj.foo;
+  delete obj.bar;
+  return obj;
+}
+
+var sampleObj = {
+  foo: 'foo', 
+  bar: 'bar', 
+  bizz: 'bizz', 
+  bang: 'bang'
+}
+
+// console.log(keyDeleter(sampleObj));
