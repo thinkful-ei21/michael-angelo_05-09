a0dda63d3bf90bf5d629763ac640a07959a7b4ed
Michael Le-Reiver <m.lereiver@gmail.com>
2018-05-09 09:03:25 -0700

    added self-reference drill

--- object-drills.js
+++ object-drills.js
@@ -29,3 +29,19 @@ function updateObject(obj)
 let oldObj = { color: 'green', foo: 'adfasdf'};
 
 // console.log(updateObject(oldObj));
+
+/*
+Modify personMaker.fullName to be a function that uses self-reference 
+(via this) in order to return the firstName and lastName properties separated by a space.
+*/
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    fullName: function() {
+      return `${this.firstName}  ${this.lastName}`;
+    }
+  };
+  return person.fullName();
+}
+// console.log(personMaker());
