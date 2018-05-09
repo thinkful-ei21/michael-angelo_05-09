b20283b4b0c22581a5c54651e1403f962602f0f3
Michael Le-Reiver <m.lereiver@gmail.com>
2018-05-09 11:10:18 -0700

    added object-initializers-and-methods

--- object-drills.js
+++ object-drills.js
@@ -57,7 +57,7 @@ var sampleObj = {
   bar: 'bar', 
   bizz: 'bizz', 
   bang: 'bang'
-}
+};
 
 // console.log(keyDeleter(sampleObj));
 
@@ -92,7 +92,7 @@ function enrollInSummerSchool(students) {
     student.status = 'In Summer school';
   });
   return students;
-};
+}
 
 const studentData = [
   {
@@ -197,3 +197,23 @@ function validateKeys(object, expectedKeys) {
   
   return true;
 }
+
+/*
+Create an object called loaf using an object initializer ({}) with two properties: 
+flour, which should be set to 300 and water which should be set to 210.
+Use console.log to print the flour and water properties.
+Add an empty method to the loaf object called hydration.
+Fill in the body of the method to return the hydration of the loaf 
+(the water divided by the flour multiplied by 100).
+Call the hydration method and use console.log to print the result.
+*/
+
+const loaf = {
+  flour: 300,
+  water: 210,
+  hydration: function() {
+    return (loaf.water / loaf.flour) * 100;
+  }
+};
+// console.log(loaf.hydration());
+
