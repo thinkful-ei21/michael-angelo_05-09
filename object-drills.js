193e779ad963272588418b90d0b7b51ea260beec
Michael Le-Reiver <m.lereiver@gmail.com>
2018-05-09 10:03:14 -0700

    added find-by-id

--- object-drills.js
+++ object-drills.js
@@ -113,3 +113,29 @@ const studentData = [
 ];
 
 // console.log(enrollInSummerSchool(studentData));
+
+/*
+This function takes 2 arguments, items and idNum. 
+items is an array of objects. 
+idNum is the id we're trying to find in items. 
+The function should look for an item with the id idNum 
+in the array items. If found, it should return that item.
+*/
+
+const scratchData = [
+  { id: 22, foo: 'bar' },
+  { id: 28, foo: 'bizz' },
+  { id: 19, foo: 'bazz' },
+];
+
+function findById(items, idNum) {
+  let newObj = {};
+  items.forEach(function (key) {
+    if (key.id === idNum ) {
+      newObj = key;
+    }
+  });
+  return newObj;
+}
+
+// console.log(findById(scratchData, 22));
